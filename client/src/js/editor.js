b79e298b142f8d04956eb451970754be2bfbb95b
Patrick Richardson Wright <wrightmight@gmail.com>
2022-08-22 21:28:08 -0700

    change links to latest codemirror version

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -14,7 +14,7 @@ export default class {
     this.editor = CodeMirror(document.querySelector('#main'), {
       value: '',
       mode: 'javascript',
-      theme: 'monokai',
+      theme: 'dracula',
       lineNumbers: true,
       lineWrapping: true,
       autofocus: true,
